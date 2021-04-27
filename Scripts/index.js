f6ef8ac9cdf8f002307fbea46a1c84ba4ada8bcc
BaysideChurch <communications@baysideonline.com>
2021-04-26 17:18:48 -0700

    added existing code

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,4 +2,16 @@
 import './api';
 import './components/date-picker';
 
-console.log('hello world, again');
+function setLoadingState(loading) {
+    if (loading) {
+        $('.bcc-loading').show();
+    } else {
+        $('.bcc-loading').hide();
+    }
+}
+
+function createElementAndSetValue(html, value) {
+    let elem = $('' + html);
+    elem.val(value);
+    return elem;
+}
